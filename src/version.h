bf2f5960020ee0c76a00b612a63aad1d8cc3c95a
Jan Tielens <jantielens@outlook.com>
2025-11-26 10:59:24 +0100

    chore: bump version to 0.0.3

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
 // Firmware version information
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 0
-#define VERSION_PATCH 2
+#define VERSION_PATCH 3
 
 // Build date (automatically set by compiler)
 #define BUILD_DATE __DATE__
