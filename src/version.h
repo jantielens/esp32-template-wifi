86cf12294b89e46d40e908bf4335df2656741de0
Jan Tielens <jantielens@outlook.com>
2025-12-26 15:37:04 +0100

    chore: bump v0.0.25 and update compile-flags docs

--- src/version.h
+++ src/version.h
@@ -6,7 +6,7 @@
 // Firmware version information
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 0
-#define VERSION_PATCH 24
+#define VERSION_PATCH 25
 
 // Build date (automatically set by compiler)
 #define BUILD_DATE __DATE__
