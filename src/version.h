57eabe160d7a1948deeaa3a806ace36c81266246
Jan Tielens <jantielens@outlook.com>
2026-01-03 14:36:53 +0100

    Bump version to 0.0.27

--- src/version.h
+++ src/version.h
@@ -6,7 +6,7 @@
 // Firmware version information
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 0
-#define VERSION_PATCH 26
+#define VERSION_PATCH 27
 
 // Build date (automatically set by compiler)
 #define BUILD_DATE __DATE__
